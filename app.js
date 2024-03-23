af63f36e6e1e09bbe9d4592f1506b64870573502
SplinterSword <57327315+SplinterSword@users.noreply.github.com>
2024-03-23 16:14:55 +0530

    Gym_Website

--- app.js
+++ app.js
@@ -18,14 +18,13 @@ app.set('views',path.join(__dirname, 'views'))
 //Mongoose Specific Stuff
 main().catch(err => console.log(err));
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/contactDance');
+  await mongoose.connect('mongodb://127.0.0.1:27017/contactGym');
 }
 const contactSchema = new mongoose.Schema({
     name: String,
-    phone: String,
     email: String,
-    address: String,
-    desc: String
+    phone: String,
+    zip: String,
 });
 const contact = mongoose.model('contact', contactSchema);
 
@@ -33,11 +32,14 @@ const contact = mongoose.model('contact', contactSchema);
 app.get('/', (req, res) => {
     res.status(200).render('home.pug');
 });
+app.get('/contact', (req, res) => {
+    res.status(200).render('contact.pug');
+});
 
 app.post('/contact',(req,res)=>{
     var myData = new contact(req.body);
     myData.save().then(()=>{
-        res.send("This item has been saved to the database")
+        res.status(200).render('contact.pug');
     }).catch(()=>{
         res.status(400).send("Item was not saved to the database");
     });
